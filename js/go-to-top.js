5c461f6472412b49711cc19bd9e0f3c5d418c8cc
Ayfri <pierre.ayfri@gmail.com>
2022-05-02 17:19:47 +0200

    feat: Add footer.

--- js/go-to-top.js
+++ js/go-to-top.js
@@ -1,3 +1,4 @@
+const footer = document.querySelector('footer');
 const goToTop = document.querySelector('#go-to-top');
 const menu = document.querySelector('#summary-list');
 const navbarMenu = document.querySelector('.navbar-menu');
@@ -8,14 +9,22 @@ function copyMenuToNavbar() {
 }
 
 function handleNavbarMenu() {
-	navbarMenuBtn.addEventListener('click', () => navbarMenu.classList.toggle('hide'));
+	navbarMenuBtn.addEventListener('click', () => navbarMenu.classList.toggle('hide-navbar'));
 	window.addEventListener('click', e => {
 		if (e.target.classList.contains('navbar-menu-btn')) return;
 		if (e.target.classList.contains('navbar-menu')) return;
-		navbarMenu.classList.add('hide');
+		navbarMenu.classList.add('hide-navbar');
 	});
 }
 
+function showFooter() {
+	let timeout;
+	window.addEventListener('scroll', () => {
+		if (timeout) clearTimeout(timeout);
+		timeout = setTimeout(() => footer.classList.remove('hide-footer'), 700);
+		footer.classList.add('hide-footer');
+	});
+}
 
 function updateGoToTopButton() {
 	if (window.scrollY > 200) {
@@ -28,6 +37,7 @@ function updateGoToTopButton() {
 window.addEventListener('load', () => {
 	copyMenuToNavbar();
 	handleNavbarMenu();
+	showFooter();
 	updateGoToTopButton();
 
 	goToTop.addEventListener('click', () => {
