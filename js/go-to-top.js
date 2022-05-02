93d7034265e842f381d89ba723f7a740554d6cfa
Ayfri <pierre.ayfri@gmail.com>
2022-05-02 16:01:38 +0200

    feat: Add navbar dropdown.

--- js/go-to-top.js
+++ js/go-to-top.js
@@ -1,27 +1,44 @@
 const goToTop = document.querySelector('#go-to-top');
+const menu = document.querySelector('#summary-list');
+const navbarMenu = document.querySelector('.navbar-menu');
+const navbarMenuBtn = document.querySelector('.navbar-menu-btn');
+
+function copyMenuToNavbar() {
+	navbarMenu.innerHTML = menu.innerHTML;
+}
+
+function handleNavbarMenu() {
+	navbarMenuBtn.addEventListener('click', () => navbarMenu.classList.toggle('hide'));
+	window.addEventListener('click', e => {
+		if (e.target.classList.contains('navbar-menu-btn')) return;
+		if (e.target.classList.contains('navbar-menu')) return;
+		navbarMenu.classList.add('hide');
+	});
+}
+
 
 function updateGoToTopButton() {
-    if (window.scrollY > 200) {
-        goToTop.classList.add('show');
-        goToTop.classList.remove('hide');
-    } else {
-        goToTop.classList.add('hide');
-        goToTop.classList.remove('show');
-    }
+	if (window.scrollY > 200) {
+		goToTop.classList.remove('hide');
+	} else {
+		goToTop.classList.add('hide');
+	}
 }
 
 window.addEventListener('load', () => {
-    updateGoToTopButton();
+	copyMenuToNavbar();
+	handleNavbarMenu();
+	updateGoToTopButton();
 
-    goToTop.addEventListener('click', () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    });
+	goToTop.addEventListener('click', () => {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth',
+		});
+	});
 
-    window.addEventListener('scroll', () => {
-        updateGoToTopButton();
-    });
+	window.addEventListener('scroll', () => {
+		updateGoToTopButton();
+	});
 
 });
